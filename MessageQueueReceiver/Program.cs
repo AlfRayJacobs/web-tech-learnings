9f0a8b3a3bc2fa218bab4927a1b9eeac0217fdb1
Adam Jacobs <ajacobs@safestyleazure.onmicrosoft.com>
2023-11-05 16:39:26 +0000

    Add message queue files

---
+++ MessageQueueReceiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Receiver started!");
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("learner", false, false, false, null);
+
+Console.WriteLine("Startup successful. Waiting for messages");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {message}");
+};
+
+while (true)
+{
+    channel.BasicConsume("learner", true, consumer);
+}
---
+++ MessageQueueSender/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("learner", false, false, false, null);
+
+while (true)
+{
+    Console.WriteLine("\nEnter message to send:");
+    var message = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(message))
+        continue;
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "learner", null, body);
+    Console.WriteLine($"Sent {message}");
+}
